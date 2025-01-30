e410c8226598d32f02ec860b07febe1486f4991d
IqboljonHasan <iqboldev@gmail.com>
2025-01-30 18:57:11 +0500

    font changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="font-bold text-5xl text-center">
+      Ваша работа мечты уже ждет вас, начните сегодня!
+    </div>
   );
 }
